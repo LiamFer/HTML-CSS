ef04a4112746859aa0e1a7b30677597a5b6baa96
William Fernandes <wiilfern1910@outlook.com>
2023-03-11 19:21:21 -0300

    JS Passport

---
+++ JS/D/ex001.js
@@ -0,0 +1,9 @@
+var vel = 12;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+
+if (vel > 60) { // Condição Simples
+  console.log("VOCÊ ULTRAPASSOU A VELOCIDADE!!");
+  console.log("Multado!");
+}
+
+console.log("Dirija sempre usando cinto de segurança!");
---
+++ JS/D/ex002.js
@@ -0,0 +1,7 @@
+var país = "EUA"
+
+if (país == "Brasil") {
+  console.log("Brasileiro")
+} else {
+  console.log(`Vivendo em ${país}, você é um Estrangeiro!`)
+}
