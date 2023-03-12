46451565f3e15799265bef6772dfe498367c7d02
William Fernandes <wiilfern1910@outlook.com>
2023-03-12 20:24:58 -0300

    JS | Repetição Ex1

--- JS/Exercícios/Repetições/EX1/script.js
+++ JS/Exercícios/Repetições/EX1/script.js
@@ -8,14 +8,17 @@ function calculate() {
   var textbox = document.getElementById("text");
   textbox.innerText = "";
 
-  if (start < 0 || end < start || step == 0) {
+  if (start.length == 0 || end.length == 0 || step.length == 0) {
     alert("Error, something is wrong!");
-  } else {
-    for (start; start < end; start = start + step) {
-      var text = document.createTextNode(`${start} e `);
-      textbox.appendChild(text);
+  } else if (start > end) {
+    for (end; start <= end; start = end -= step) {
+      textbox.innerText += ` ${start} \u{1F449}`;
     }
-    var finaltxt = document.createTextNode("Fim");
-    textbox.appendChild(finaltxt);
+    textbox.innerText += `\u{1F9A7}`;
+  } else {
+    for (start; start <= end; start = start + step) {
+      textbox.innerText += ` ${start} \u{1F449}`;
+    }
+    textbox.innerText += `\u{1F9A7}`;
   }
 }
