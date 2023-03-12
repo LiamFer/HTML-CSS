78a1fa05269c85d3114c8528f3d04bf67ace9212
William Fernandes <wiilfern1910@outlook.com>
2023-03-12 20:36:06 -0300

    JS | Repetição EX2

--- JS/Exercícios/Repetições/EX2/script.js
+++ JS/Exercícios/Repetições/EX2/script.js
@@ -2,9 +2,19 @@ var button = document.getElementById("button");
 button.addEventListener("click", generator);
 
 function generator() {
-  for (var start = 1; start <= 10; start++) {
-    var number = Number(document.getElementById("number").value);
-    var solve = number * start;
-    console.log(`${number} X ${start} = ${solve} `);
+  var tab = document.getElementById("seltab");
+  var number = Number(document.getElementById("number").value);
+  tab.innerHTML = "";
+  if (number.length == 0) {
+    alert("Please type a number");
+  } else {
+    for (var start = 1; start <= 10; start++) {
+      var number = Number(document.getElementById("number").value);
+      var solve = number * start;
+      var text = document.createElement("option");
+      text.text = `${number} X ${start} = ${solve}`;
+      text.value = `tab${start}`;
+      tab.appendChild(text);
+    }
   }
 }
