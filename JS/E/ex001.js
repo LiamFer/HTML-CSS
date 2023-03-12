56fb9057eafc4a7323e41b533fb5167a7ab77284
William Fernandes <wiilfern1910@outlook.com>
2023-03-12 17:30:01 -0300

    JS | For , While , Do While

---
+++ JS/E/ex001.js
@@ -0,0 +1,13 @@
+var pizza = 8;
+
+do {
+  console.log(`Comi o pedaço ${pizza}`);
+  pizza--;
+} while (pizza > 0);
+
+number = 5
+
+for (var start = 1; start <= 10; start++){
+    var times = number * start
+    console.log(times)
+}
