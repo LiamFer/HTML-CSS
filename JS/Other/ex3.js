7809d120efe3147f3cd371ea4ca4379e6ed71d30
William Fernandes <wiilfern1910@outlook.com>
2023-06-10 10:55:18 -0300

    EX3

--- JS/Other/ex3.js
+++ JS/Other/ex3.js
@@ -1,27 +1,34 @@
-const metros = parseFloat(
-  prompt("Insira um valor em metros para ser convertido:")
-);
-let uniMedida = parseFloat(prompt(
-  "Para qual unidade você deseja converter? (1 mm/ 2 cm/ 3 dm/ 4 dam/ 5 hm/ 6 km)"
-))
+let medida = parseFloat(prompt("Insira um valor em Metros"));
 
-switch (uniMedida) {
-  case 1:
-    alert("A conversão de metros para mm é" + metros * 1000);
+const choice = prompt(
+  "Quer converter esse valor para quanto?\na) milímetro\nb) centímetro\nc) decímetro\nd) decâmetro\ne) hectômetro\nf) quilômetro"
+);
+
+switch (choice) {
+  case "a":
+    medida *= 1000;
+    alert(`A conversão de metros para milímetros é ${medida}`);
     break;
-  case 2:
-    alert("A conversão de metros para cm é" + metros * 100);
+  case "b":
+    medida *= 100;
+    alert(`A conversão de metros para centímetros é ${medida}`);
     break;
-  case 3:
-    alert("A conversão de metros para dm é" + metros * 10);
+  case "c":
+    medida *= 10;
+    alert(`A conversão de metros para decímetro é ${medida}`);
     break;
-  case 4:
-    alert("A conversão de metros para dam é" + metros * 0.1);
+  case "d":
+    medida /= 10;
+    alert(`A conversão de metros para decâmetro é ${medida}`);
     break;
-  case 5:
-    alert("A conversão de metros para hm é" + metros * 0.01);
+  case "e":
+    medida /= 100;
+    alert(`A conversão de metros para hectômetro é ${medida}`);
     break;
-  case 6:
-    alert("A conversão de metros para km é" + metros * 0.001);
+  case "f":
+    medida /= 1000;
+    alert(`A conversão de metros para quilômetro é ${medida}`);
     break;
+  default:
+    alert("Essa opção não existe");
 }
