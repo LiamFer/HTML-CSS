fe6c860ebf99588296a26e038aaef31b0cb98f8f
William Fernandes <wiilfern1910@outlook.com>
2023-06-27 18:25:56 -0300

    Update functions.js

--- JS/JS-Moderno/functions.js
+++ JS/JS-Moderno/functions.js
@@ -23,7 +23,6 @@ const mediaPonderada = (...numbers) => {
   return sum / weight;
 };
 
-
 // Os valores tem que ser passados assim console.log(mediaPonderada([10,2],[5,1]))
 
 // Mediana
@@ -43,6 +42,7 @@ const mediana = (...numbers) => {
 };
 
 // Moda
+// Forma de pegar a moda usando um array
 
 const moda = (...numbers) => {
   let copy = [];
@@ -61,4 +61,26 @@ const moda = (...numbers) => {
   return numbers[copy.indexOf(maxNumber)];
 };
 
+// Forma de pegar a moda usando um objeto
 
+const moda2 = (...numbers) => {
+  let obj = {};
+  numbers.forEach((numbers) => (obj[numbers] = 0));
+  let objArr = Object.keys(obj);
+  numbers.forEach((number) => {
+    for (let i = 0; i < numbers.length; i++) {
+      if (number == objArr[i]) obj[String(number)] += 1;
+    }
+  });
+  let moda = 0;
+  let key = 0;
+  for (let i in obj) {
+    if (obj[i] > moda) {
+      moda = obj[i];
+      key = i;
+    }
+  }
+  let index = objArr.indexOf(key)
+  return console.log(objArr[index]);
+};
+console.log(moda2(3,3,5,9,3,2,1,3,3,5,15,18));
